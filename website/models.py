a2f40f6fff0da0e3ce4bf3a97dca7c98094fac34
Robbi Nespu <robbinespu@gmail.com>
2021-11-11 05:44:01 +0800

    ➕ added indieweb kinds

--- website/models.py
+++ website/models.py
@@ -5,6 +5,24 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
+    INDIEWEB_KINDS = (
+        ("article", "article"),
+        ("status", "status"),
+        ("in-reply-to", "in-reply-to"),
+        ("rsvp", "rsvp"),
+        ("like-of", "like-of"),
+        ("bookmark", "bookmark"),
+        ("checkin", "checkin"),
+        ("repost-of", "repost-of"),
+        ("photo", "photo"),
+        ("sport", "sport"),
+    )
+    kind = models.CharField(
+        max_length=13,
+        choices=INDIEWEB_KINDS,
+        default="article",
+    )
+
     intro = models.TextField()
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
