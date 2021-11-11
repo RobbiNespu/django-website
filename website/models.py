9ef5ab53e714a4fdb522eb122b7c572447c16b79
Robbi Nespu <robbinespu@gmail.com>
2021-11-11 10:13:32 +0800

    🥎 use foreignkey to call dropdown value

--- website/models.py
+++ website/models.py
@@ -2,30 +2,25 @@ from django.db import models
 
 
 # Create your models here.
+class IndiewebKind(models.Model):
+    kind = models.CharField(max_length=14)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_added']
+
+    def __str__(self):
+        return self.kind
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    INDIEWEB_KINDS = (
-        ("article", "article"),
-        ("status", "status"),
-        ("in-reply-to", "in-reply-to"),
-        ("rsvp", "rsvp"),
-        ("like-of", "like-of"),
-        ("bookmark", "bookmark"),
-        ("checkin", "checkin"),
-        ("repost-of", "repost-of"),
-        ("photo", "photo"),
-        ("sport", "sport"),
-    )
-    kind = models.CharField(
-        max_length=13,
-        choices=INDIEWEB_KINDS,
-        default="article",
-    )
-
-    intro = models.TextField()
+    kind = models.ForeignKey(IndiewebKind, on_delete=models.CASCADE)
+    intro = models.TextField(max_length=150)
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-date_added']
+
