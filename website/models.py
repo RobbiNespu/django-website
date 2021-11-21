5da753dcfaec102f9076dc967eec074ba3c9e723
Robbi Nespu <robbinespu@gmail.com>
2021-11-21 21:55:19 +0800

    ✨DB changes for CKEditor

--- website/models.py
+++ website/models.py
@@ -1,3 +1,4 @@
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
 
@@ -23,15 +24,27 @@ class IndiewebSource(models.Model):
     def __str__(self):
         return self.source
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     kind = models.ForeignKey(IndiewebKind, on_delete=models.CASCADE)
     source = models.ForeignKey(IndiewebSource, on_delete=models.CASCADE,null=True)
-    intro = models.TextField(max_length=150)
-    body = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
+    intro = models.TextField(max_length=200)
+    body = RichTextUploadingField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    modification_date = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     class Meta:
-        ordering = ['-date_added']
+        ordering = ['-creation_date']
+        get_latest_by = 'creation_date'
+        indexes = [
+            models.Index(fields=['-creation_date'])
+        ]
+
