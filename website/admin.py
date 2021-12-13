01ba7276305c2ac11057c3e631e26586408b9b89
Robbi Nespu <robbinespu@gmail.com>
2021-12-13 15:14:09 +0800

    💅 customize the way data is displayed in the administration panel

--- website/admin.py
+++ website/admin.py
@@ -6,6 +6,9 @@ from .models import Post, Tag
 class PostAdmin(admin.ModelAdmin):
     radio_fields = {"source": admin.HORIZONTAL}
     list_display = ('title', 'slug', 'status', 'creation_date')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
 
 
 admin.site.register(Post, PostAdmin)
